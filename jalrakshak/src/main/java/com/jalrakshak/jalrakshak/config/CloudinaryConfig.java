e9fb6b3469ea4a2a6309d5f83df3b921c99d810f
arya stark <aryastark@got.com>
2026-07-13 16:20:38 +0530

    Cloduinary print removed

--- jalrakshak/src/main/java/com/jalrakshak/jalrakshak/config/CloudinaryConfig.java
+++ jalrakshak/src/main/java/com/jalrakshak/jalrakshak/config/CloudinaryConfig.java
@@ -28,11 +28,6 @@ public class CloudinaryConfig {
 		String cleanApiKey = apiKey.trim();
 		String cleanApiSecret = apiSecret.trim();
 
-		System.out.println("Cloudinary Cloud Name = " + cleanCloudName);
-		System.out.println("Cloudinary API Key = " + cleanApiKey);
-		System.out.println("Cloudinary API Secret Length = " + cleanApiSecret.length());
-		System.out.println("Cloudinary API Secret First 2 = " + cleanApiSecret.substring(0, 2));
-		System.out.println("Cloudinary API Secret Last 2 = " + cleanApiSecret.substring(cleanApiSecret.length() - 2));
 
 		Map<String, String> config = ObjectUtils.asMap("cloud_name", cleanCloudName, "api_key", cleanApiKey,
 				"api_secret", cleanApiSecret, "secure", "true");
